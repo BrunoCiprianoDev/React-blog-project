ea2393813411d5c66dff8cc1c525923c28b5d983
Bruno Cipriano <cipriano990@gmail.com>
2022-12-01 17:23:55 -0300

    Concluindo links para usuário autenticado

--- blog-project/src/App.js
+++ blog-project/src/App.js
@@ -1,5 +1,15 @@
 import './App.css';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+
+
+//Hooks
+import {useState, useEffect} from 'react'
+import { useAuthentication } from './hooks/useAutentication';
+
+
+//Context
+import { AuthProvider } from './context/AuthContext';
 
 //Pages
 import About from './pages/About/About.js';
@@ -10,14 +20,28 @@ import Register from './pages/Register/Register';
 //Components
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
-
-
-
-
-
+import CreatePost from './pages/CreatePost/CreatePost';
+import Dashboard  from './pages/Dashboard/Dashboard'
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
+
+  const loadingUser = user === undefined;
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user)=> {
+      setUser(user)
+    })
+  }, [auth])
+
+  if(loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
+      <AuthProvider value={{user}}>
       <BrowserRouter>
         <Navbar />
         <div className='container'>
@@ -26,10 +50,13 @@ function App() {
             <Route path="/about" element={<About/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/register" element={<Register/>}/>
+            <Route path="/posts/create" element={<CreatePost/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
           </Routes>
         </div>
         <Footer />
       </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
--- blog-project/src/components/navbar/Navbar.js
+++ blog-project/src/components/navbar/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 
+import { useAuthentication } from '../../hooks/useAutentication'
+import {useAuthValue} from '../../context/AuthContext'
+
 import styles from './Navbar.module.css'
 
+
+
 const Navbar = () => {
+
+    const {user} = useAuthValue();
+
   return (
     <nav className={styles['navbar']}>
         <NavLink to="/" className={styles['brand']}>
@@ -13,12 +21,26 @@ const Navbar = () => {
             <li>
                 <NavLink to="/"  className={({ isActive }) => (isActive ? styles.active : "")}>Home</NavLink>
             </li>
-            <li>
-                <NavLink to="/login"  className={({ isActive }) => (isActive ? styles.active : "")}>Entrar</NavLink>
-            </li>
-            <li>
-                <NavLink to="/register"  className={({ isActive }) => (isActive ? styles.active : "")}>Cadastrar</NavLink>
-            </li>
+           {!user && (
+                <>
+                    <li>
+                        <NavLink to="/login"  className={({ isActive }) => (isActive ? styles.active : "")}>Entrar</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/register"  className={({ isActive }) => (isActive ? styles.active : "")}>Cadastrar</NavLink>
+                    </li>
+                </>
+           )}
+           {user && (
+                <>
+                    <li>
+                        <NavLink to="/posts/create"  className={({ isActive }) => (isActive ? styles.active : "")}>Novo post</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard"  className={({ isActive }) => (isActive ? styles.active : "")}>Dashboard</NavLink>
+                    </li>    
+                </>
+           )}
             <li>
                 <NavLink to="/about"  className={({ isActive }) => (isActive ? styles.active : "")}>Sobre</NavLink>
             </li>
